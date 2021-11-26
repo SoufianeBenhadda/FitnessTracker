cf375c67b22575d98d029cac549836d4351dd3e7
Mahacine <ettahirimahacine@gmail.com>
2021-11-26 02:11:55 +0100

    Database connection established

--- app/src/main/java/com/example/fitnesstracker/dao/UserDao.java
+++
@@ -1,8 +0,0 @@
-package com.example.fitnesstracker.dao;
-
-import com.example.fitnesstracker.model.User;
-
-public interface UserDao {
-
-    public User authenticate(String username, String password);
-}
--- app/src/main/java/com/example/fitnesstracker/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.example.fitnesstracker.service;
-
-import com.example.fitnesstracker.model.User;
-
-public interface UserService {
-
-    public User authenticate(String username, String password);
-}
