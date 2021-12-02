a4b89bb0b7672ba64a0248292ca8796fedabd10e
Soufiane Benhadda <benhadda.soufiane1@gmail.com>
2021-12-02 15:43:02 +0100

    test

---
+++ app/src/main/java/com/example/fitnesstracker/CustomListAdapter.java
@@ -0,0 +1,82 @@
+package com.example.fitnesstracker;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.fitnesstracker.model.Excercise;
+
+import java.util.List;
+
+public class CustomListAdapter  extends BaseAdapter {
+
+    private List<Excercise> listData;
+    private LayoutInflater layoutInflater;
+    private Context context;
+
+    public CustomListAdapter(Context aContext,  List<Excercise> listData) {
+        this.context = aContext;
+        this.listData = listData;
+        layoutInflater = LayoutInflater.from(aContext);
+    }
+
+    @Override
+    public int getCount() {
+        return listData.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return listData.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+        if (convertView == null) {
+            convertView = layoutInflater.inflate(R.layout.activity_card, null);
+            holder = new ViewHolder();
+            holder.exoIcon = (ImageView) convertView.findViewById(R.id.imageView_Exoicon);
+            holder.exoTitle = (TextView) convertView.findViewById(R.id.textView_Title);
+            holder.exoDesc = (TextView) convertView.findViewById(R.id.textView_Desc);
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+        Excercise exo = this.listData.get(position);
+        holder.exoTitle.setText(exo.getTitle());
+        holder.exoDesc.setText(exo.getDescription());
+
+        //int imageId = this.getMipmapResIdByName(country.getFlagName());
+
+        //holder.flagView.setImageResource(imageId);
+
+        return convertView;
+    }
+
+    // Find Image ID corresponding to the name of the image (in the directory mipmap).
+    public int getMipmapResIdByName(String resName)  {
+        String pkgName = context.getPackageName();
+        // Return 0 if not found.
+        int resID = context.getResources().getIdentifier(resName , "mipmap", pkgName);
+        Log.i("CustomListView", "Res Name: "+ resName+"==> Res ID = "+ resID);
+        return resID;
+    }
+
+    static class ViewHolder {
+        ImageView exoIcon;
+        TextView exoTitle;
+        TextView exoDesc;
+    }
+
+}
--- app/src/main/java/com/example/fitnesstracker/Home.java
+++ app/src/main/java/com/example/fitnesstracker/Home.java
@@ -2,13 +2,68 @@ package com.example.fitnesstracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.example.fitnesstracker.dao.HomeDao;
+import com.example.fitnesstracker.dao.LoginDao;
+import com.example.fitnesstracker.model.Excercise;
+import com.example.fitnesstracker.model.User;
+
+import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 public class Home extends AppCompatActivity {
-
+    private List<Excercise> exos;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        List<Excercise> image_details = getListData();
+        final ListView listView = (ListView) findViewById(R.id.listView);
+        listView.setAdapter(new CustomListAdapter(this, image_details));
+
+        // When the user clicks on the ListItem
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> a, View v, int position, long id) {
+                Object o = listView.getItemAtPosition(position);
+                Excercise exo = (Excercise) o;
+                Toast.makeText(getApplicationContext(), "Selected :" + " " + exo, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private List<Excercise> getListData() {
+        try {
+            exos=new HomeDao().execute().get();
+        }
+        catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+        catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        if(exos==null){
+            Toast toast=Toast.makeText(getApplicationContext(),"No data",Toast.LENGTH_LONG);
+            toast.show();
+            Log.d("myerror","Error");
+        }
+        else{
+
+            Toast toast=Toast.makeText(getApplicationContext(),"yes data",Toast.LENGTH_LONG);
+            toast.show();
+            Log.d("logged","Logged");
+
+        }
+        return exos;
+
+
     }
 }
